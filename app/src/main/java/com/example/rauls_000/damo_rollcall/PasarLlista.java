643e5fbc2307a552c530c13f11c0abe2a3e01b53
Raúl Sanchez Monera <raulsanchezmonera@hotmail.com>
2017-12-28 20:32:29 +0100

    Terminado el fragment de calendario

--- app/src/main/java/com/example/rauls_000/damo_rollcall/PasarLlista.java
+++ app/src/main/java/com/example/rauls_000/damo_rollcall/PasarLlista.java
@@ -14,8 +14,6 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
-
-
 public class PasarLlista extends Activity {
 
     public EditText editData;
@@ -30,7 +28,6 @@ public class PasarLlista extends Activity {
 
             @Override
             public void onClick(View arg0) {
-
                 DialogFragment newFragment = new SelectDateFragment();
                 newFragment.show(getFragmentManager(), "DatePicker");
 
@@ -40,25 +37,7 @@ public class PasarLlista extends Activity {
                 android.R.layout.simple_spinner_dropdown_item, grupos);
     }
 
-}
-
-
-public class SelectDateFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
-
-    @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-        final Calendar calendar = Calendar.getInstance();
-        int yy = calendar.get(Calendar.YEAR);
-        int mm = calendar.get(Calendar.MONTH);
-        int dd = calendar.get(Calendar.DAY_OF_MONTH);
-        return new DatePickerDialog(getActivity(), this, yy, mm, dd);
-    }
-
-    public void onDateSet(DatePicker view, int yy, int mm, int dd) {
-        populateSetDate(yy, mm+1, dd);
-    }
-    public void populateSetDate(int year, int month, int day) {
+    public void setdate(int year, int month, int day) {
         editData.setText(month+"/"+day+"/"+year);
     }
-
 }

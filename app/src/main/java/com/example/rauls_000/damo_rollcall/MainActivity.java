f37157cb93a639207dba152ab3a19f0664d0b53b
Raúl Sanchez Monera <raulsanchezmonera@hotmail.com>
2017-12-28 13:23:03 +0100

    Testing commit

--- app/src/main/java/com/example/rauls_000/damo_rollcall/MainActivity.java
+++ app/src/main/java/com/example/rauls_000/damo_rollcall/MainActivity.java
@@ -1,13 +1,118 @@
 package com.example.rauls_000.damo_rollcall;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+//import android.support.v4.widget.DrawerLayout;
+//import android.support.v7.app.ActionBarDrawerToggle;
+//import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.widget.Toolbar;
+//import android.view.Menu;
+//import android.view.MenuItem;
 
 public class MainActivity extends Activity {
+    //private DrawerLayout mDrawerLayout;
+    //private ActionBarDrawerToggle mDrawerToggle;
+    //private CharSequence mDrawerTitle;
+    //private CharSequence mTitle;
+
+    ImageButton addbuttton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addbuttton = (ImageButton) findViewById(R.id.add_button);
+        addbuttton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PasarLlista.class);
+                startActivity(intent);
+            }
+        });
+
+
+        //Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        //setSupportActionBar(myToolbar);
+
+        /*
+        //Drawer
+        mTitle = mDrawerTitle = getTitle();
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,                  *//* host Activity *//*
+                mDrawerLayout,         *//* DrawerLayout object *//*
+                R.string.drawer_open,  *//* "open drawer" description *//*
+                R.string.drawer_close  *//* "close drawer" description *//*
+        ) {
+
+            *//* Called when a drawer has settled in a completely closed state. *//*
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getActionBar().setTitle(mTitle);
+            }
+
+            *//* Called when a drawer has settled in a completely open state. *//*
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getActionBar().setTitle(mDrawerTitle);
+            }
+        };
+
+        // Set the drawer toggle as the DrawerListener
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);*/
     }
+
+    /*@Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+    */
+
+    /*
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.actionbar, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        *//*if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        // Handle your other action bar items...
+        return super.onOptionsItemSelected(item);*//*
+
+        //This is for the simpler action bar
+
+        switch (item.getItemId()) {
+            case R.id.my_toolbar:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+
+    }*/
 }
